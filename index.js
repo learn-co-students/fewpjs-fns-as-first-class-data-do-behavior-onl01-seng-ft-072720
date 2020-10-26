ac406d27fa12198648bc260a54347fed02258afe
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-26 16:39:12 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let timeInt = parseInt(time.split(":"));
+
+  if (timeInt < 12) {
+    return 'Good Morning';
+  } else if (timeInt > 17) {
+    return 'Good Evening';
+  } else {
+    return 'Good Afternoon';
+  }
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
 /* Write your implementation of displayMessage() */
